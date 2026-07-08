0bfc946a1bbba8e3f224cad8902629e21b38deb4
benjaminagogo1 <josephbenjamin046@gmail.com>
2026-07-08 10:21:47 +0100

    Python basics

---
+++ data_types.py
@@ -0,0 +1,44 @@
+# string data types
+
+first = "Benjamin"
+last = "Agogo"
+print(type(first))
+print(type(first) == str)
+print(isinstance(last, str))
+
+# concatenation
+
+fullname = "otete Benjamin" " Agogo"
+# print(fullname)
+
+fullname += "!"
+print(fullname)
+
+decade = str(2000)
+print(type(decade))
+
+statement = "I like soulful gospel songs from " + decade + "s."
+print(statement)
+
+# Escaping Specail Characters
+
+sentence = "I'm back at work\ntHey!\n\ntcome"
+print(sentence)
+
+print("")
+print("")
+
+print(first)
+print(first.lower())
+print(first.upper())
+print(last.replace("Agogo", "Otete"))
+last = "Otete"
+print(fullname)
+
+print("")
+
+title = "menu".upper()
+print(title.center(20, "="))
+print("Bread".ljust(16, ".") + " #200")
+print("Fish".ljust(16, ".") + " #500")
+print("Oil".ljust(16, ".") + " #1000")
