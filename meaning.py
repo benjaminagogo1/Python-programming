cbd3f08aff9312cde35a33063a7c60940441201f
benjaminagogo1 <josephbenjamin046@gmail.com>
2026-07-08 09:27:31 +0100

    python basics

--- meaning.py
+++ meaning.py
@@ -2,7 +2,11 @@ meaning = 10
 print("")
 # print(meaning)
 
-if meaning > 10:
-    print("Right on!")
-else:
-    print("Not today")
+# if meaning > 10:
+#     print("Right on!")
+# else:
+#     print("Not today")
+
+
+# Ternary Operator
+print("Right on") if meaning > 10 else print("Not today")
