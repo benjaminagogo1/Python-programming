8036703ca435a987f88a0f23ddbdd83b70ff9791
benjaminagog1 <josephbenjamin046@gmail.com>
2026-07-09 02:13:01 +0100

    Expense Tracker

--- Expense-tracker/main.py
+++ Expense-tracker/main.py
@@ -1,11 +1,46 @@
-print("Welcome To My Expense Tracker.\nA companion that never forgets where your money go!")
-print("")
-userOption = input("Choose....\n1. Add Expense\n2. Show Expense\n3. Exit\n")
+expenses = []
 
-if userOption != "1" and userOption != "2" and userOption != "3":
-    print("You must choose between 1, 2 or 3")
+print("Welcome To My Expense Tracker.\nA companion that never forgets where your money goes!")
+print()
 
-option = int(userOption)
+running = True
+while running:
 
-if option == 1:
-    print("Expense Name..")
+    userOption = input("Choose....\n1. Add Expense\n2. Show Expense\n3. Exit\n")
+
+
+    if userOption != "1" and userOption != "2" and userOption != "3":
+        print("You must choose between 1, 2 or 3")
+
+    option = int(userOption)
+
+    if option == 1:
+        expenseName = input("Expense Name..\n")
+        amount = input("Expense Amount..\n")
+        userAmount = int(amount)
+        expense = {
+            "name": expenseName,
+            "amount": userAmount,
+        }
+        expenses.append(expense)
+        print(expenses)
+    elif option == 2:
+        get()
+
+    elif option == 3:
+        running = False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
