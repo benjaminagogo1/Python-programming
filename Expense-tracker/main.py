e9dd6529a9ff4d34eff7b926c60522eb120287b6
benjaminagogo1 <josephbenjamin046@gmail.com>
2026-07-11 22:27:32 +0100

    Expense tracker

--- Expense-tracker/main.py
+++ Expense-tracker/main.py
@@ -38,9 +38,10 @@ def show_expense():
     for expense in expenses:
         if len(expense["name"]) > longest_name:
             longest_name = len(expense["name"])
-    print(f"{"N0":<{longest_name}} {"Name":<{longest_name}} {"AMount":<{longest_name}} {"Date":<{longest_name}}")
+    print(f"{"N0"} {"Name":<{longest_name}}{"Amount":<{longest_name}}{"Date":>{longest_name}}")
+    print("-")
     for expense in expenses:
-        print(f"{number}. {expense["name"]:<{longest_name}}| ₦{expense["amount"]:<{longest_name}}| {expense["date"]:<{longest_name}}|")
+        print(f"{number}. {expense["name"]:<{longest_name}} ₦{expense["amount"]:<{longest_name}} {expense["date"]:<{longest_name}}")
         number += 1
         total += expense["amount"]
     print()
