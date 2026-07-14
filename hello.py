4a954bc8f8ee788dd8a3516eaf46bab0b802f1f9
benjaminagogo1 <josephbenjamin046@gmail.com>
2026-07-14 10:38:29 +0100

    python f-string

--- hello.py
+++ hello.py
@@ -42,3 +42,16 @@ price = 60
 text = f"The total is {price:.2f} dollars"
 
 print(text)
+
+#A backslash followed by three integers will result in a octal value:
+txt = "\110\145\154\154\157"
+print(txt) 
+
+
+txt = "Hello\rWorld!"
+print(txt) 
+
+
+#A backslash followed by an 'x' and a hex number represents a hex value:
+txt = "\x48\x65\x6c\x6c\x6f"
+print(txt) 
