5e4a79d74e889883b0cbc9d5585f1ddf91d93a9f
benjaminagogo1 <josephbenjamin046@gmail.com>
2026-07-15 23:24:20 +0100

    My contact-Book

--- contact-book/main.py
+++ contact-book/main.py
@@ -1,23 +1,34 @@
 contacts = []
 
+y = True
+while y:
 
 
-contact_name = input("Enter the contact name\n")
 
-try:
-    contact_phone = int(input("Enter the phone number\n"))
-except ValueError:
-    print("Error: Please enter only digit.")
-    # continue
-contact = {
-    "name": contact_name,
-    "phone": contact_phone,
-}
+    contact_name = input("Enter the contact name\n")
 
-contacts.append(contact)
-for contact in contacts:
-    print(f"contact Number: {contact["name"]}")
-    print(f"contact phone: {contact["phone"]}")
-      
+    try:
+        contact_phone = int(input("Enter the phone number\n"))
+    except ValueError:
+        print("Error: Please enter only digit.")
+        # continue
+    contact = {
+        "name": contact_name,
+        "phone": contact_phone,
+    }
 
-
+    contacts.append(contact)
+    print("added successfully.")
+    print()
+    
+    while True:
+        add_choice = input("Do you want to another contact?\n1. Yes\n2. No\n")
+        try:
+            add_choice = int(add_choice)
+        except ValueError:
+            print("Invalid input: Please enter only digit.\n")
+            continue
+        if add_choice == 1:
+            break
+        # if add_choice == 2:
+        #     return
