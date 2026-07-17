53b7520390c43a0d7533c497a90a8fab7b364a8f
benjaminagogo1 <josephbenjamin046@gmail.com>
2026-07-17 09:32:13 +0100

    Contact book

--- contact-book/main.py
+++ contact-book/main.py
@@ -91,11 +91,24 @@ while running:
                 continue
             selected_contact["Contact-Phone"] = new_contact_phone
     elif chosen_option == 4:
+        serial = 1
         if not contacts:
             print("Not contact found.")
             continue
         for delete_contact in contacts:
-            print(delete_contact)
+            print(f"{serial}. {delete_contact['Contact-Name']} - {delete_contact['Contact-Phone']}")
+            serial += 1
+        item_selected = input("Choose the contact number to delete\n")
+        if not item_selected.isdigit():
+            print("Invalid input: Only digits are allowed.")
+            continue
+        item_selected = int(item_selected)
+        item_selected_index = item_selected -1
+        if item_selected_index < 0 or item_selected_index > len(contacts):
+            print("Invalid input: Choose from the list of contacts.")
+            continue
+        item_deleted = contacts.pop(item_selected_index)
+        print(f"{item_deleted["Contact-Name"]}: deleted successfully!")
     elif chosen_option == 5:
         running = False
         break
